8c7714ab4584a62dca4657a711cb32693222f454
surawatthana <surawatthana@hotmail.com>
2020-09-03 17:18:58 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,5 @@ export class AppComponent {
 
   constructor(private messageService: MessageService) { }
 
-  addSingle() {
-    this.messageService.add({ severity: 'success', summary: 'Service Message', detail: 'Via MessageService' });
-  }
 
-  addMultiple() {
-    this.messageService.addAll(
-      [
-        { severity: 'success', summary: 'Service Message', detail: 'Via MessageService' },
-    { severity: 'info', summary: 'Info Message', detail: 'Via MessageService' }]);
-  }
-
-  clear() {
-    this.messageService.clear();
-  }
 }
