44c9738dd6b4900796d16592212bd15eb1eb3028
surawatthana <surawatthana@hotmail.com>
2020-09-03 17:34:48 +0700

    login page generated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { FormsModule } from '@angular/forms';
 
 import { ToastModule } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ToastModule,ButtonModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ToastModule,ButtonModule, 
+  RouterModule.forRoot([
+     {path:'', component: LoginComponent}
+  ])],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
